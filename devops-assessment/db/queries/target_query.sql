a83773a0dca8dd52436a133bee3dec2704f749eb
saisuryateja1234 <122426676+saisuryateja1234@users.noreply.github.com>
2026-07-08 13:02:12 +0530

    Update target_query.sql

--- devops-assessment/db/queries/target_query.sql
+++ devops-assessment/db/queries/target_query.sql
@@ -1,10 +1,3 @@
--- The query Part 5 asks us to optimize.
--- Run with EXPLAIN ANALYZE to see the index from
--- 001_create_tables.sql (idx_hotel_bookings_city_created_at) being used:
---
---   docker compose exec db psql -U app_admin -d hotelbook \
---     -c "EXPLAIN ANALYZE $(cat db/queries/target_query.sql)"
-
 SELECT org_id, status, COUNT(*), SUM(amount)
 FROM hotel_bookings
 WHERE city = 'delhi'
